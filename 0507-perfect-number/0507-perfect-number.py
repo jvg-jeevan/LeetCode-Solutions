a656cf7649c2f55433b25794e28646908e98638f
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2025-09-09 08:51:37 +0530

    Time: 4 ms (62.74%), Space: 17.8 MB (63.61%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -4,18 +4,15 @@ class Solution:
             return False
         sum = 1
         i = 2
-        # get divisors until sqrt(n)
+        # get divisors until sqrt(n) in terms of pairs (x, y)
         while i*i <= num:
             if num%i == 0:
                 sum += i
+                # if x divides num then y should also divide num
+                # inequality check to avoid duplicating values
                 if i != num//i:
                     sum += (num//i)
             i += 1
 
-        # # finding divisor from sqrt(n) to n excluded
-        # while i>1:
-        #     if num%i == 0:
-        #         sum += (num//i)
-        #     i -= 1
         print(sum)
         return sum == num
