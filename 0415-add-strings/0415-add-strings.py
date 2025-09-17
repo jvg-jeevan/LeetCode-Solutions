f7bcd5fe25c3f42abd9cee4498a3af8bad1c31bc
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2025-09-17 21:31:05 +0530

    Time: 47 ms (5.08%), Space: 18.1 MB (54.56%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,16 +1,19 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        # base case
         if num1 == '0' and num2 == '0':
             return '0'
 
+
         num = self.str_to_int(num1) + self.str_to_int(num2)
         res = []
+        # converting num to string
         while num > 0:
             res.insert(0, chr((num%10) + 48))
             num //= 10
         return ''.join(res)
 
-
+    # string to integer
     def str_to_int(self, n):
         num = 0
         for i in n:
