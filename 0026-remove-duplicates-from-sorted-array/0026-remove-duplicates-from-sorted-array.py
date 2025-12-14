35b8364d6c4733feef41383540207605a9fbf2b8
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2025-12-14 14:09:34 +0530

    Time: 1 ms (59.44%), Space: 19.1 MB (28.95%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # use two pointer one to read and other to write
+        # in read if previous element same as current then skip
+        # if not then place the current read in the write position and update write
         if not nums:
             return 0
         write = 1
