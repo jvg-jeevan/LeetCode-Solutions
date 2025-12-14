f62a15b7ed29caf7905da3fae84da757887bbc66
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2025-12-14 14:31:03 +0530

    Time: 3 ms (48.01%), Space: 19 MB (28.95%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,7 +7,7 @@ class Solution:
             return 0
         write = 1
         for read in range(1, len(nums)):
-            if nums[read] != nums[read - 1]:
+            if nums[read] != nums[write - 1]:
                 nums[write] = nums[read]
                 write += 1
         return write
