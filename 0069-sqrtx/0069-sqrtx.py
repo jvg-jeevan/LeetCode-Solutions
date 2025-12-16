e233082bfe7c1b1a3b53d19fc681157c7569b3bc
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2025-12-16 17:25:03 +0530

    Time: 3 ms (62.75%), Space: 17.9 MB (10.03%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,5 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        # same way as binary search
+        # take the mid and see if perfect square if not continue
         low, high = 0, x
         while low <= high:
             mid = (low + high) // 2
@@ -9,4 +11,5 @@ class Solution:
                 high = mid-1
             else:
                 low = mid+1
+        # when low>high return high because it is the closest sqrt as low is already reached higher
         return high
