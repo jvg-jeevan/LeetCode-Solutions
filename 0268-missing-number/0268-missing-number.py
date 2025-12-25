bbf2375be63bba6144a8add4e34246f8a331005e
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2025-12-25 22:20:56 +0530

    Time: 6 ms (38.63%), Space: 18.4 MB (93.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        expected = (n * (n+1)) // 2
-        return expected - sum(nums)
+        # n = len(nums)
+        # expected = (n * (n+1)) // 2
+        # return expected - sum(nums)
+
+        # a ^ a = 0 a ^ 0 = a
+        # xor all indices and all integers in nums results in missing number
+
+        xor = 0
+        for i in range(len(nums)):
+            xor ^= i
+            xor ^= nums[i]
+
+        xor ^= len(nums)
+        return xor
