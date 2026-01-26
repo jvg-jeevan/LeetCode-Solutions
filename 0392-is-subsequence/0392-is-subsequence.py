4848c3e5025afa73b7dff57051854cbf49f8d1f5
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-01-26 16:09:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (33.43%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,6 +4,7 @@ class Solution:
         m, n = len(t), len(s)
         i, j = 0, 0
         # if the matching is complete then the value of j will be equal to len of s
+        # if not subsequence then the length will be lesser
         while i < m and j < n:
             if s[j] != t[i]:
                 i += 1
