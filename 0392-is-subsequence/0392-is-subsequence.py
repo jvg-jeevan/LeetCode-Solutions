6e1c319a25a7e3de68692c5c71fbb67bd7d8676c
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-01-26 16:08:41 +0530

    Time: 4 ms (9.51%), Space: 19.4 MB (24.15%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # try matching char in longest string t with each in s
+        m, n = len(t), len(s)
+        i, j = 0, 0
+        # if the matching is complete then the value of j will be equal to len of s
+        while i < m and j < n:
+            if s[j] != t[i]:
+                i += 1
+            else:
+                i += 1
+                j += 1
+        return j == n
