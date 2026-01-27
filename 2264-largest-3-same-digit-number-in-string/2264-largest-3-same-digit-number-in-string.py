5265d0999aa2d36a26141c1a1b972b5f4bec5a77
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-01-27 06:27:49 +0530

    Time: 4 ms (29.62%), Space: 19.3 MB (24.84%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
@@ -1,7 +1,10 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
+        # res to store all the triples
         res = []
+        # checking whether the next 3 are tiples if there add to res
         for i in range(len(num)-2):
             if num[i] == num[i+1] == num[i+2]:
                 res.append(num[i] + num[i+1] + num[i+2])
+        # return the highest value
         return max(res) if res else ''
