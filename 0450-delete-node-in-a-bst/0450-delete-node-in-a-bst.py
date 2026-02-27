c070cac9291aafb8e5bcfc1fd808fc48370458a3
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-02-27 22:06:01 +0530

    Time: 3 ms (20.06%), Space: 21.9 MB (80.81%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMin(self, node):
+        while node.left != None:
+            node = node.left
+        return node
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root == None:
+            return None
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left == None and root.right == None:
+                return None
+            
+            if root.left == None:
+                return root.right
+            if root.right == None:
+                return root.left
+            
+            successor = self.findMin(root.right)
+            root.val = successor.val
+            root.right = self.deleteNode(root.right, successor.val)
+        
+        return root
