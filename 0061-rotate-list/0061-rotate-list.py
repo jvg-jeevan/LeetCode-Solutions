83aee292f87613d54e8b06ae767f2f37bc18afae
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-03-07 15:38:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,25 +5,52 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if head == None:
-            return None
-        if head.next == None:
+        if head == None or head.next == None:
             return head
 
-        n = 0
-        node = head
-        while node != None:
-            node = node.next
+        n = 1
+        tail = head
+        while tail.next != None:
+            tail = tail.next
             n += 1
-        
-        k = k % n
 
-        for _ in range(k):
-            cur = head
-            while cur.next.next != None:
-                cur = cur.next
-            node = cur.next
-            cur.next = None
-            node.next = head
-            head = node
-        return head
+        k = k % n
+        if k == 0:
+            return head
+        
+        tail.next = head
+
+        steps = n-k-1
+        new_tail = head
+        for _ in range(steps):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
+        
+        return new_head
+
+
+
+        # if head == None:
+        #     return None
+        # if head.next == None:
+        #     return head
+
+        # n = 0
+        # node = head
+        # while node != None:
+        #     node = node.next
+        #     n += 1
+        
+        # k = k % n
+
+        # for _ in range(k):
+        #     cur = head
+        #     while cur.next.next != None:
+        #         cur = cur.next
+        #     node = cur.next
+        #     cur.next = None
+        #     node.next = head
+        #     head = node
+        # return head
