f4807aa7a2177ace365cffa7dacbd967db3765f4
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-08-01 11:32:30 +0530

    Time: 69 ms (41.28%), Space: 25.5 MB (16.71%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,6 +1,22 @@
-from math import sqrt
+import heapq
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key = lambda point: point[0]**2 + point[1]**2)
-        return points[:k]
+        # here sorting based on the closest distance using lambda function
+        # points.sort(key = lambda point: point[0]**2 + point[1]**2)
+        # return points[:k]
+
+        # using minheap
+
+        heap = []
+        # calculate distance and push to minheap as python only supports maxheap push in -ve
+        for x, y in points:
+            dist = x ** 2 + y ** 2
+            heapq.heappush(heap, (dist, x, y))
+        
+        res = []
+        for _ in range(k):
+            _, x, y = heapq.heappop(heap)
+            res.append([x, y])
+
+        return res
