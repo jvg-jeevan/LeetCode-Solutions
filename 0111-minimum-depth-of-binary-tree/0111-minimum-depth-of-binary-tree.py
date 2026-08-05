e87e152fda1ba52af3d1b842ef8f8f30a44b57da
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-08-05 07:29:00 +0530

    Time: 44 ms (33.06%), Space: 45 MB (54.93%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -7,25 +7,19 @@
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
 
-        def dfs(node):
-            if node == None:
-                return 0
-# if leaf node return 1
-            if node.left == None and node.right == None:
-                return 1
+        if root == None:
+            return 0
 
 # get the left and right side depth
-            left_side = dfs(node.left)
-            right_side = dfs(node.right)
+        left_side = self.minDepth(root.left)
+        right_side = self.minDepth(root.right)
+
 # this is because if skewed to one side it should not give only root as depth
 # if left side is null take right and viceversa
-            if left_side == 0:
-                return 1 + right_side
+        if left_side == 0:
+            return 1 + right_side
 
-            elif right_side == 0:
-                return 1 + left_side
+        if right_side == 0:
+            return 1 + left_side
 
-            else:
-                return 1 + min(left_side, right_side)
-
-        return dfs(root)
+        return 1 + min(left_side, right_side)
