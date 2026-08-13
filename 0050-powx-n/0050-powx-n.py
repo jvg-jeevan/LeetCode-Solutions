edef9a6fd0acad5e4aea3516a7e3f0d06c0b54c8
Jeevan V Gowda <161923062+jvg-jeevan@users.noreply.github.com>
2026-08-13 06:42:35 +0530

    Time: 0 ms (100%), Space: 19.5 MB (55.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,14 +1,20 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # if odd x*x^(n-1) = x^n
-        # if even x^n = (x^(n/2))^2
+        
+        def power(x, n):
+            
+            if n == 0:
+                return 1
+
+            half = power(x, n // 2)
+
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+
         if n < 0:
-            x = 1/x
+            x = 1 / x
             n = -n
-        res = 1
-        while n > 0:
-            if n%2 != 0:
-                res *= x
-            x = x*x
-            n //= 2
-        return res
+
+        return power(x, n)
